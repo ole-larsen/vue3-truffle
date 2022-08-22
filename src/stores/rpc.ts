dec7625f02b7ba80b4e8432a705542f9e05a17dc
Ole Larsen <ole.larssen777@gmail.com>
2022-08-22 18:05:49 +0000

    add backend provider

---
+++ src/stores/rpc.ts
@@ -0,0 +1,61 @@
+import {getCurrentInstance} from "vue";
+import { defineStore } from "pinia";
+
+export const useRpcStore = defineStore({
+  id: "rpc",
+  state: () => ({
+    provider:   getCurrentInstance()?.appContext.config.globalProperties.$rpc,
+    registered: false,
+    connected:  false,
+    accounts:   [""],
+    address:    "",
+    chainID:    0,
+    nodeInfo:   ""
+  }),
+  getters: {
+    isRegistered:  (state) => state.registered,
+    isConnected:   (state) => state.connected,
+    checkMetaMask: (state) => false,
+    getChainID:    (state) => state.chainID,
+    getAccounts:   (state) => state.accounts,
+    getNodeInfo:   (state) => state.nodeInfo
+  },
+  actions: {
+    register() {
+      if (!this.provider) {
+        return this.registered = false
+      }
+      this.registered = true
+
+      return this.storeChainId()
+          .then((chainID: number | PromiseLike<number>) => {
+            this.connected = true
+            this.chainID = chainID as number
+            console.log(this.chainID)
+          
+            return this.chainID
+          })
+          .catch((e: Error) => {
+            this.registered = false
+            this.connected = false
+            throw e
+          })
+    },
+    storeAddress(accounts: string[]) {
+      return accounts[0]
+    },
+    storeAccounts() {
+      this.provider("http://localhost:1111/api/v1/accounts")
+        .then((response: Response) => response.json())
+        .then((accounts: string[]) => {
+          console.log(accounts)
+          this.accounts = accounts
+        })
+        .catch((e: Error) => {throw e})
+    },
+    storeChainId() {
+      return this.provider("http://localhost:1111/api/v1/chain_id")
+        .then((response: Response) => response.json())
+    }
+  }
+})
